9c4b2f497a41fb4406b8b639842a0a48ff9a468e
Jada-Marie <jadamariead@gmail.com>
2024-10-29 09:23:32 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const board = document.querySelector("#board");
   const squares = board.querySelectorAll("div");
 
-  squares.forEach(square => {
+  squares.forEach((square) => {
     square.classList.add("square");
+  });
+
+  const state = Array(9).fill(null); 
+  let currentPlayer = "X";
+
+  squares.forEach((square, index) => {
+    square.addEventListener("click", () => {
+
+      if (state[index] == null ) {
+
+        state[index] = currentPlayer;
+
+        square.textContent = currentPlayer; 
+        square.classList.add(currentPlayer); 
+
+        if (currentPlayer == "X"){
+          currentPlayer = "O";
+        }else{
+          currentPlayer = "X";
+        }
+      }
+    });
   });
 });
