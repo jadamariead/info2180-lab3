467bfe1aa742da64638ed582f9b3c82820da2de2
Jada-Marie <jadamariead@gmail.com>
2024-10-29 15:22:26 -0500

    Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const board = document.querySelector("#board");
   const squares = board.querySelectorAll("div");
+  const statusDiv = document.getElementById("status");
+  const newGameBtn = document.getElementById("btn")
 
   squares.forEach((square) => {
     square.classList.add("square");
   });
 
-  const state = Array(9).fill(null); 
+  let state = [null, null, null, null, null, null, null, null, null];
   let currentPlayer = "X";
 
   squares.forEach((square, index) => {
+    
     square.addEventListener("mouseover",() =>{
       square.classList.add("hover");
     });
@@ -26,13 +29,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
         square.textContent = currentPlayer; 
         square.classList.add(currentPlayer); 
-
+        
         if (currentPlayer == "X"){
           currentPlayer = "O";
         }else{
           currentPlayer = "X";
         }
+
+        let winner = checkForWinner(state);
+
+        if (winner == "X") {
+          statusDiv.textContent = "Congratulations! X is the Winner!";
+          statusDiv.classList.add("you-won");
+        }
+        else if (winner == "O")
+        {
+          statusDiv.textContent = "Congratulations! O is the Winner!";
+          statusDiv.classList.add("you-won");
+        }  
       }
     });
   });
+
+  function checkForWinner(state){
+    if ((state[0] == "X" && state[1] == "X" && state[2] == "X") || (state[3] == "X" && state[4] == "X" && state[5] == "X") || (state[6] == "X" && state[7] == "X" && state[8] == "X") || (state[0] == "X" && state[3] == "X" && state[6] == "X") || (state[1] == "X" && state[4] == "X" && state[7] == "X") || (state[2] == "X" && state[5] == "X" && state[8] == "X") || (state[0] == "X" && state[4] == "X" && state[8] == "X") || (state[2] == "X" && state[4] == "X" && state[6] == "X"))
+    {
+      return "X";
+    } 
+    else if ((state[0] == "O" && state[1] == "O" && state[2] == "O") || (state[3] == "O" && state[4] == "O" && state[5] == "O") || (state[6] == "O" && state[7] == "O" && state[8] == "O") || (state[0] == "O" && state[3] == "O" && state[6] == "O") || (state[1] == "O" && state[4] == "O" && state[7] == "O") || (state[2] == "O" && state[5] == "O" && state[8] == "O") || (state[0] == "O" && state[4] == "O" && state[8] == "O") || (state[2] == "O" && state[4] == "O" && state[6] == "O"))
+    {
+      return "O";
+    } 
+    return null
+  }
+
 });
+
